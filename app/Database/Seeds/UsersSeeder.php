3cdbe6abe4ce8ee87f3ecad1a8a24e56dac7e9c3
Ivan Zarkovic <ivan.zarkovic@ewe.rs>
2022-03-23 16:42:33 +0100

    Plant seeds

--- app/Database/Seeds/UsersSeeder.php
+++ app/Database/Seeds/UsersSeeder.php
@@ -3,11 +3,26 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
+use Faker\Factory;
 
 class UsersSeeder extends Seeder
 {
     public function run()
     {
-        //
+        for ($i = 0; $i < 1000; $i++) { //to add 10 clients. Change limit as desired
+            $this->db->table('user')->insert($this->generateFakeUserData());
+        }
+    }
+
+    private function generateFakeUserData(): array
+    {
+        $faker = Factory::create();
+        return [
+            'username' => $faker->userName(),
+            'first_name' => $faker->firstName(),
+            'last_name' => $faker->lastName(),
+            'phone' => $faker->phoneNumber(),
+            'email' => $faker->email
+        ];
     }
 }
